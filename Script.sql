c34262f7c8061360d5b4ac889f1fedd7d8e8c5e9
Duc <85634478+SnenzZ@users.noreply.github.com>
2023-09-28 23:36:11 +0200

    Delete Script.sql

--- Script.sql
+++
@@ -1,8 +0,0 @@
--- Inspecting Data
-SELECT * FROM sales_data_sample
-
--- Check unique values
-SELECT DISTINCT status FROM sales_data_sample
-SELECT DISTINCT year_id FROM sales_data_sample
-SELECT DISTINCT qtr_id FROM sales_data_sample
-SELECT DISTINCT month_id FROM sales_data_sample
