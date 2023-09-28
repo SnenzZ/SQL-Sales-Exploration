9b363b75645a82d08b3247f6be591e39bd23cfb0
Duc <85634478+SnenzZ@users.noreply.github.com>
2023-09-28 16:23:26 +0200

    Add files via upload
    Commit Init

---
+++ Script.sql
@@ -0,0 +1,8 @@
+-- Inspecting Data
+SELECT * FROM sales_data_sample
+
+-- Check unique values
+SELECT DISTINCT status FROM sales_data_sample
+SELECT DISTINCT year_id FROM sales_data_sample
+SELECT DISTINCT qtr_id FROM sales_data_sample
+SELECT DISTINCT month_id FROM sales_data_sample
